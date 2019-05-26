1be4d87cf2113235d6f2bdabc7ed4d3de6e79b94
Andrew White <github@and.rew.ninja>
2019-05-26 19:56:03 +1000

    Initial commit

---
+++ lib/supportpal/ruby.rb
@@ -0,0 +1,8 @@
+require "supportpal/ruby/version"
+
+module Supportpal
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/supportpal/ruby/version.rb
@@ -0,0 +1,5 @@
+module Supportpal
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
